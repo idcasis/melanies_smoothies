a74c9ea55be0df2b18c8aad01f575e4fa365ec74
idcasis <idcasis@outlook.com>
2026-07-21 14:41:47 -0500

    Update streamlit_app.py
    rest api integrations

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import os
 from snowflake.snowpark.functions import col 
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
